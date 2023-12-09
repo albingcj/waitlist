691f469f41eb5934ae89bb96517f9e6615bb9b7e
albingcj <114584909+albingcj@users.noreply.github.com>
2023-12-09 11:57:00 +0530

    button

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
-$(document).ready(function () {
-    $("#list").DataTable();
+$(document).ready(function() {
+    $('.registerBtn').click(function() {
+        var waitType = $(this).data('waitType');
+
+        // Set the value of the modal input field
+        $('#waitType').val(waitType);
+
+        // Open the modal if it's not open already
+        $('#regModal').modal('show');
+    });
 });
