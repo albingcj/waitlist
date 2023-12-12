15b07870c4dbcb74c532699545b212d3a4f0bc59
albingcj <114584909+albingcj@users.noreply.github.com>
2023-12-12 16:38:28 +0530

    Update waitlist.sql schema and data

--- waitlist.sql
+++ waitlist.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2023 at 10:59 AM
+-- Generation Time: Dec 12, 2023 at 12:07 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -33,17 +33,38 @@ CREATE TABLE `accordion` (
   `subhead` varchar(255) NOT NULL,
   `type` varchar(255) NOT NULL,
   `content` text NOT NULL,
-  `image` varchar(255) NOT NULL
+  `image` text NOT NULL,
+  `total` int(11) NOT NULL DEFAULT 0,
+  `queue` int(11) NOT NULL DEFAULT 0,
+  `status` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `accordion`
 --
 
-INSERT INTO `accordion` (`id`, `name`, `subhead`, `type`, `content`, `image`) VALUES
-(1, 'iphone 14 Pro', 'Join waitlist for Pro version.', 'IPHN14P', 'The iphone 14 pro is the most premium version u can get from the market.', ''),
-(2, 'iphone SE 3', 'Join Waitlist to get the newest SE', 'IPHNSE3', 'the iphone se is the smallest yet powerful se you can buy', ''),
-(3, 'samsung S11', 'the latest samsung phone in the market', 'SMSNG11', 'The samsung s11 is the popular samsung product we have. join the waitlist to join the queue', '');
+INSERT INTO `accordion` (`id`, `name`, `subhead`, `type`, `content`, `image`, `total`, `queue`, `status`) VALUES
+(1, 'iphone 12', 'Join the waitlist to grab your Iphone 12', 'IPHN12', 'Iphone 12 is the latest product we lauch. join this waitlist to grab one for you when it is available.', 'https://pngimg.com/uploads/iphone_12/iphone_12_PNG3.png', 2, 2, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `name`, `email`, `password`) VALUES
+(1, 'admin', 'admin@a', '1');
 
 -- --------------------------------------------------------
 
@@ -65,43 +86,7 @@ CREATE TABLE `table1` (
 
 INSERT INTO `table1` (`id`, `userid`, `name`, `count`, `flag`) VALUES
 (1, 1, 'Albin', 101, 0),
-(2, 3, 'w', 101, 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `table2`
---
-
-CREATE TABLE `table2` (
-  `id` int(11) NOT NULL,
-  `userid` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `count` int(11) NOT NULL DEFAULT 100,
-  `flag` int(11) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `table2`
---
-
-INSERT INTO `table2` (`id`, `userid`, `name`, `count`, `flag`) VALUES
-(1, 1, 'Albin', 100, 0),
-(2, 3, 'w', 101, 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `table3`
---
-
-CREATE TABLE `table3` (
-  `id` int(11) NOT NULL,
-  `userid` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `count` int(11) NOT NULL DEFAULT 100,
-  `flag` int(11) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+(2, 2, 'a', 100, 0);
 
 -- --------------------------------------------------------
 
@@ -119,9 +104,8 @@ CREATE TABLE `userlist` (
 --
 
 INSERT INTO `userlist` (`id`, `joinedTables`) VALUES
-(1, ',1,2'),
-(2, ''),
-(3, ',1,2');
+(1, ',1'),
+(2, ',1');
 
 -- --------------------------------------------------------
 
@@ -141,9 +125,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
-(1, 'Albin Gerorge C J', '927622mca003@mkce.ac.in', '$2y$10$XRucBzHDqzvdQo3SDUtB/e4/5uQ/mO4C2UM3OtHS6kUhtw8F.I8f6'),
-(2, 'q', 'q@q', '$2y$10$oGmWYCFT7zyQ9c9dL1XHiOXK5nd5/mnJfsW4qQwwPDiTB7xSKyVT6'),
-(3, 'w', 'w@w', '$2y$10$wn5Oc7AIQ4.uK2E23O2NMOPRvSyVLAKV3JESpEx.41jmPHquOmvxe');
+(1, 'Albin george c j', '927622mca003@mkce.ac.in', '$2y$10$t9xrHSQf1pC04ZNx1tztzeyf7DGt2zfzqWVs5NxSZ3AJSmU3I79Pe'),
+(2, 'a', 'a@a', '$2y$10$wWYWWPIlmwqw6IGm/rtJBONfY9c.gO55w7LeIHtxQpL71C6QUMsz2');
 
 --
 -- Indexes for dumped tables
@@ -156,21 +139,15 @@ ALTER TABLE `accordion`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `table1`
 --
 ALTER TABLE `table1`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `table2`
---
-ALTER TABLE `table2`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `table3`
---
-ALTER TABLE `table3`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -193,37 +170,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `accordion`
 --
 ALTER TABLE `accordion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `table1`
 --
 ALTER TABLE `table1`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `table2`
---
-ALTER TABLE `table2`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `table3`
---
-ALTER TABLE `table3`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `userlist`
 --
 ALTER TABLE `userlist`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
