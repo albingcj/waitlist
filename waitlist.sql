6fcdcfd1d0d655fb6aadf38ea5d9e1b38bb580fc
albingcj <114584909+albingcj@users.noreply.github.com>
2023-12-13 16:55:09 +0530

    Update waitlist.sql with new products and modified admin email

--- waitlist.sql
+++ waitlist.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 12, 2023 at 12:07 PM
+-- Generation Time: Dec 13, 2023 at 12:24 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -44,7 +44,9 @@ CREATE TABLE `accordion` (
 --
 
 INSERT INTO `accordion` (`id`, `name`, `subhead`, `type`, `content`, `image`, `total`, `queue`, `status`) VALUES
-(1, 'iphone 12', 'Join the waitlist to grab your Iphone 12', 'IPHN12', 'Iphone 12 is the latest product we lauch. join this waitlist to grab one for you when it is available.', 'https://pngimg.com/uploads/iphone_12/iphone_12_PNG3.png', 2, 2, 0);
+(1, 'Apple 2023 MacBook Pro ', 'Apple 2023 MacBook Pro (16-inch, M3 Max chip with 16‑core CPU and 40‑core GPU, 48GB Unified Memory, 1TB) - Space Black ', 'MacBook Pro', 'narain', 'https://pngimg.com/uploads/macbook/macbook_PNG51.png', 2, 2, 1),
+(2, 'Apple 2022 12.9-inch iPad Pro', 'Apple 2022 12.9-inch iPad Pro (Wi-Fi + Cellular, 2TB) - Silver (6th Generation) ', 'iPad Pro', 'Unleash your creative potential with the Apple 2022 12.9-inch iPad Pro in sleek Silver. This 6th generation powerhouse boasts a massive 2TB SSD, letting you store oceans of inspiration and work seamlessly offline. Wi-Fi and cellular connectivity keep you tethered to the creative cloud, while the blazing-fast M2 chip tackles demanding tasks with effortless grace. Immerse yourself in the vibrant colors and razor-sharp detail of the Liquid Retina XDR display, the perfect canvas for your imagination. Whether you\'re a graphic designer, filmmaker, or simply a power user, this iPad Pro is your gateway to boundless possibilities.', 'https://images.frandroid.com/wp-content/uploads/2021/04/apple-ipad-pro-129-2021-frandroid.png', 1, 1, 0),
+(3, 'Apple iPhone 15 Pro Max', 'Apple iPhone 15 Pro Max (1 TB) - Blue Titanium ', 'iPhone 15 Pro Max', 'Bathed in the future\'s glow, the iPhone 15 Pro Max in Blue Titanium whispers luxury. Its aerospace-grade shell houses a ProMotion display that dances with vibrant life, while the A17 Bionic chip, a beast within, unleashes power unheard. Capture moments in breathtaking detail with the revolutionary camera system, then disappear into the future with augmented reality and seamless connectivity. This phone isn\'t just technology; it\'s a statement. Own the future.', 'https://static1.pocketnowimages.com/wordpress/wp-content/uploads/2023/09/pbi-iphone-15-pro-max.png', 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -64,7 +66,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `name`, `email`, `password`) VALUES
-(1, 'admin', 'admin@a', '1');
+(1, 'admin', 'admin@waitlist.com', 'Admin@1');
 
 -- --------------------------------------------------------
 
@@ -86,7 +88,42 @@ CREATE TABLE `table1` (
 
 INSERT INTO `table1` (`id`, `userid`, `name`, `count`, `flag`) VALUES
 (1, 1, 'Albin', 101, 0),
-(2, 2, 'a', 100, 0);
+(2, 2, 'George', 100, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table2`
+--
+
+CREATE TABLE `table2` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `count` int(11) NOT NULL DEFAULT 100,
+  `flag` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `table2`
+--
+
+INSERT INTO `table2` (`id`, `userid`, `name`, `count`, `flag`) VALUES
+(1, 2, 'George', 100, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table3`
+--
+
+CREATE TABLE `table3` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `count` int(11) NOT NULL DEFAULT 100,
+  `flag` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -105,7 +142,7 @@ CREATE TABLE `userlist` (
 
 INSERT INTO `userlist` (`id`, `joinedTables`) VALUES
 (1, ',1'),
-(2, ',1');
+(2, ',1,2');
 
 -- --------------------------------------------------------
 
@@ -125,8 +162,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
-(1, 'Albin george c j', '927622mca003@mkce.ac.in', '$2y$10$t9xrHSQf1pC04ZNx1tztzeyf7DGt2zfzqWVs5NxSZ3AJSmU3I79Pe'),
-(2, 'a', 'a@a', '$2y$10$wWYWWPIlmwqw6IGm/rtJBONfY9c.gO55w7LeIHtxQpL71C6QUMsz2');
+(1, 'Albin george c j', '927622mca003@mkce.ac.in', '$2y$10$pam/TJbSXjvzY3TtHNXTPOhUEe1GNhZZ5a4vCFAUMwVfIMu.XdeEq'),
+(2, 'George c j', 'albingcj@gmail.com', '$2y$10$M1aoavQ/r3klixanFkWHm.LStOpzWn7ozboAwfKyPfXUCoMeyYYo6');
 
 --
 -- Indexes for dumped tables
@@ -151,6 +188,18 @@ ALTER TABLE `table1`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `table2`
+--
+ALTER TABLE `table2`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `table3`
+--
+ALTER TABLE `table3`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `userlist`
 --
 ALTER TABLE `userlist`
@@ -170,7 +219,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `accordion`
 --
 ALTER TABLE `accordion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `admin`
@@ -183,6 +232,18 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `table1`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `table2`
+--
+ALTER TABLE `table2`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `table3`
+--
+ALTER TABLE `table3`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `userlist`
